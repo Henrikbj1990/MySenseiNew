12a72dbcfa2a791b24df0a8381e22d7f2a9ed833
Julian <vanjarjar@gmail.com>
2015-12-01 00:25:22 +0100

    Added a Profile view for Users.

---
+++ MySensei/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MySensei.Infrastructure;
+using MySensei.Models;
+
+namespace MySensei.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        [Authorize]
+        public ActionResult Index()
+        {
+            //var currentUserId = User.Identity.GetUserId();
+            var manager = new UserManager<AppUser>(new UserStore<AppUser>(new AppIdentityDbContext()));
+            var currentUser = manager.FindById(User.Identity.GetUserId());
+
+            return View(currentUser);
+        }
+    }
+}
