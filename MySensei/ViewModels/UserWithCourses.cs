54305bdc90145fba0a8800bfb7c333c8037e95df
Julian <vanjarjar@gmail.com>
2015-12-01 22:02:47 +0100

    Add a viewmodel user and courses

---
+++ MySensei/ViewModels/UserWithCourses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySensei.Models;
+
+namespace MySensei.ViewModels
+{
+    public class UserWithCourses
+    {
+        public AppUser AppUser { get; set; }
+        public Course Course { get; set; }
+        public AssignedTagData AssignedTagData { get; set; }
+    }
+}
