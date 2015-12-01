057d6d5a2999e6e554d33232e78444cc56a08012
Julian van Enckevort <vanjarjar@gmail.com>
2015-12-01 22:00:34 +0100

    Moved database-related methods to a new Repository-file.

--- MySensei/Controllers/CoursesController.cs
+++ MySensei/Controllers/CoursesController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 using MySensei.Infrastructure;
@@ -9,14 +8,21 @@ using MySensei.Models;
 
 namespace MySensei.Controllers
 {
-
-    public class HomeController : Controller
+    public class CoursesController : Controller
     {
         private readonly Repository _repository = new Repository();
+
         // GET: Home
         public ActionResult Index()
         {
             return View(_repository.GetCourses());
         }
+
+        [HttpPost]
+        public ActionResult SearchCourses(string SearchString)
+        {
+            return View(_repository.SearchCourses(SearchString).ToList());
+        }
+
     }
 }
---
+++ MySensei/Models/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using MySensei.Infrastructure;
+
+namespace MySensei.Models
+{
+    public class Repository
+    {
+        private AppIdentityDbContext db = new AppIdentityDbContext();
+
+        // GET: Courses
+        public IEnumerable<Course> GetCourses()
+        {
+
+            return db.Courses.ToList();
+        }
+
+        internal Course GetCourseById(int id)
+        {
+            return db.Courses.FirstOrDefault(x => x.CourseID == id);
+        }
+
+        public IQueryable<Course> SearchCourses(string searchString)
+        {
+            if (String.IsNullOrEmpty(searchString))
+            {
+                searchString = "";
+            }
+
+            return db.Courses.Where(s => s.Title.Contains(searchString));
+        }
+    }
+}
