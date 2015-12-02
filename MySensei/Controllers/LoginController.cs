89b653b8e1675b1cdd722c731ef522f971e65c5e
Julian <vanjarjar@gmail.com>
2015-12-03 00:36:12 +0100

    Added partial view for login (not working)

---
+++ MySensei/Controllers/LoginController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using MySensei.Infrastructure;
+using MySensei.Models;
+
+namespace MySensei.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl = "/Home")
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return View("Error", new string[] { "Access Denied" });
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return PartialView("_Login");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel details, string returnUrl = "/Home")
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await UserManager.FindAsync(details.UserName, details.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Invalid name or password.");
+                }
+                else
+                {
+                    ClaimsIdentity ident = await UserManager.CreateIdentityAsync(user,
+                    DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthManager.SignOut();
+                    AuthManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = false
+                    }, ident);
+
+                    return Redirect(returnUrl);
+                }
+            }
+            ViewBag.returnUrl = returnUrl;
+            return PartialView("_Login", details);
+        }
+
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            AuthManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private IAuthenticationManager AuthManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+    }
+}
