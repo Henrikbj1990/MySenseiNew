964520530ea418bfbe48084243e02aace5cd37bf
Daniel Skram <dnlskrm@gmail.com>
2015-12-02 22:23:29 +0100

    SCSS Styling & start front-end

---
+++ MySensei/Scripts/init.js
@@ -0,0 +1,15 @@
+﻿function init() {
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 70,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header, "small");
+        } else {
+            if (classie.has(header, "small")) {
+                classie.remove(header, "small");
+            }
+        }
+    });
+}
+window.onload = init();
